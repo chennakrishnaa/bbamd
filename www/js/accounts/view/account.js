0554632690f08f7d2a04b00f23a283d9ffdbf74d
Venkatarao Gali <kriti@gali-2.local>
2013-06-10 18:32:46 +0200

    initial commit

--- www/js/accounts/view/account.js
+++ www/js/accounts/view/account.js
@@ -1,8 +1,10 @@
 define(
 	["backbone","rivetsconfig", 
-	"account/collections/account","text!account/template/account.html","pagebus" ],
-	function(Backbone,rivets, accountCollection,template,Pagebus){
+	"account/collections/account","text!account/template/account.html","pagebus" ,"account/helpers/viewhelpers"],
+	function(Backbone,rivets, accountCollection,template,Pagebus,viewHelpers){
 		return Backbone.View.extend ({
+			template: _.template(template),
+			el: $('#accounts'),
 			initialize: function () {
 				var view = this.view,
 				//set model
@@ -22,9 +24,9 @@ define(
 					 view.updateModel(param.userid);
 				 });
 				//load the application html
-				el=this.$el;
+				//el=this.$el;
 				//append the el
-				this.$el.append(template);
+				//this.$el.append(template);
 				//console.log(this.$el);
 				//render the accounts only once the page is loaded
 				this.listenToOnce(this.model, 'add', this.render);
@@ -34,17 +36,22 @@ define(
 				
 			},
 			events:{
-				'click #account-list li':'publishEvent'
+				'click li':'publishEvent'
 			},
 			//render the template
 			render: function() {
 					//render template
-					rivets.bind(this.$el,{accounts:this.model})
+					//rivets.bind(this.$el,{accounts:this.model})
 					//this.$el.html(this.template(this.model.attributes));
+					var data = this.model.toJSON();
+					_.extend(data,viewHelpers)
+					console.log(viewHelpers);
+					$(this.el).html(this.template({accounts:data}));
 					
 			},
 			publishEvent: function(el) {
 				//console.log($(el))
+				el.preventDefault();
 				console.log($(el.target).attr('id'));
 				Pagebus.publish("showmovements", {"accountid":$(el.target).attr('id')})
 			},
